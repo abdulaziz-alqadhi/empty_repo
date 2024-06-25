02d5687ea56886c9193c3330625218fcfb7a3e10
[Abdulaziz A.H Alqadhi] <[abdulazizalqadhi1s1s@gmail.com]>
2024-06-26 00:05:45 +0300

    first lesson basic types and variables

--- intro/basic_types_and_vars.kt
+++ intro/basic_types_and_vars.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("This is Basic Types and Varibles Practice")   
+    println("This is Basic Types and Varibles Practice") 
+    val name = "abdulaziz"
+    val gender = 'M'
+    val age = 21
+    val bloodGroup = "o+"
+    var hight = 170.0f
+    val weight = 52.50
+    var married = false
 } 
