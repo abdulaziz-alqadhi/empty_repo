709ff5dd66fadc3fdb0c35f4c7deecf28dcac939
[Abdulaziz A.H Alqadhi] <[abdulazizalqadhi1s1s@gmail.com]>
2024-06-27 00:07:25 +0300

    added loops kotlin practice file

--- intro/conditions.kt
+++ intro/conditions.kt
@@ -1,3 +1,20 @@
 fun main() {
-    print("This is Conditions Practice")
+    println("This is Conditions Practice")
+    val c =  12
+    val h = c/2
+
+    
+    for (i in 1..h-1) {
+        for (j in 1..i) {
+            print("*")
+        }
+        println()
+    }
+
+    for (m in h downTo 1) {
+        for (k in 1..m) {
+            print("*")
+        }
+        println()
+    }
 }
--- intro/loops.kt
+++ intro/loops.kt
@@ -1,3 +1,18 @@
 fun main() {
-    print("This is Loops Practice")
+    println("This is Loops Practice\n")
+    val h = 6
+    
+    for (i in 1..h-1) {
+        for (j in 1..i) {
+            print("*")
+        }
+        println()
+    }
+
+    for (m in h downTo 1) {
+        for (k in 1..m) {
+            print("*")
+        }
+        println()
+    }
 }
